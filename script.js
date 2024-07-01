054dad396b542b25450b7a29c93a42b73d6aa8d1
Matthew Wanjiku <imattheaven@gmail.com>
2024-07-01 06:09:17 +0300

    added eval() function

--- script.js
+++ script.js
@@ -2,4 +2,7 @@ const display = document.getElementById('display');
 
 function calculate() {
     console.log(display.value);
+    let answer = eval(display.value);
+    console.log(answer);
+    display.value = answer;
 }
